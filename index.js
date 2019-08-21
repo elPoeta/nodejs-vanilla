747603ccd3f47cf8f813a499fe044efafa04085e
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-08-21 09:19:46 -0300

    log request url and method

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimedPath = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLowerCase();
   res.end('Response from vanilla nodejs server\n');
+  console.log(`URL: ${trimedPath} | Method: ${method}`);
 });
 
 const PORT = process.env.PORT || 5000;
