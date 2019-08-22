953f484238a8c5675331081bfa19010ea2ea826a
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-08-22 09:08:25 -0300

    log query and headers

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const server = http.createServer((req, res) => {
   const path = parsedUrl.pathname;
   const trimedPath = path.replace(/^\/+|\/+$/g, "");
   const method = req.method.toLowerCase();
+  const stringQuery = parsedUrl.query;
+  const headers = req.headers;
+
   res.end('Response from vanilla nodejs server\n');
-  console.log(`URL: ${trimedPath} | Method: ${method}`);
+  console.log(`URL: ${trimedPath} | Method: ${method} | Query: ${JSON.stringify(stringQuery)}`);
+  console.log(`Headers: ${JSON.stringify(headers)}`);
 });
 
 const PORT = process.env.PORT || 5000;
