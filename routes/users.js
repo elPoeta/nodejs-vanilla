aa69f34f691fcfeb7306eace7663fa2b45b62538
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-08-24 17:10:41 -0300

    add post put and delete user handler

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,30 @@ module.exports = (data, callback) => {
   const method = methods.indexOf(data.method) > -1 ? data.method : 'default';
 
   const userGet = () => {
-    callback(200, { name: 'Leonardo' });
+    callback(200, { getU: 'READ OK' });
   }
+
+  const userPost = () => {
+    callback(200, { postU: 'CREATE OK' });
+  }
+
+  const userPut = () => {
+    callback(200, { putU: 'UPDATE OK' });
+  }
+
+  const userDelete = () => {
+    callback(200, { delU: 'DELETE OK' });
+  }
+
   const invalidMethod = () => {
     callback(400);
   }
 
   const chosenMethod = {
     get: userGet,
+    post: userPost,
+    put: userPut,
+    delete: userDelete,
     deafult: invalidMethod
   }
 
