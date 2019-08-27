7a5976ba71db8f1f58c864c3a598bbb506f49d6d
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-08-26 22:23:29 -0300

    add post token handler

--- routes/tokens.js
+++ routes/tokens.js
@@ -1,17 +1,49 @@
 const dataStore = require('../lib/data');
-const { hashPassword } = require('../lib/helpers');
+const { hashPassword, createRandomString } = require('../lib/helpers');
 
 module.exports = (data, callback) => {
   const methods = ['get', 'post', 'put', 'delete'];
-  const method = methods[data.method] > -1 ? data.method : 'default';
+  const method = methods.indexOf(data.method) > -1 ? data.method : 'default';
 
   const tokenGet = () => {
     callback(200, { ok: 'Get token' });
   }
 
   const tokenPost = () => {
-    callback(200, { ok: 'Post token' });
+    console.log('tokenPost')
+    const { phone, password } = data.payload;
+    dataStore.read('users', phone.trim(), (err, userData) => {
+      if (err) {
+        callback(404, { error: "User not found" });
+        return;
+      }
 
+      const hashedPassword = hashPassword(password.trim());
+      if (hashedPassword != userData.password) {
+        callback(403, { error: 'Invalid Password' });
+        return;
+      }
+
+      const id = createRandomString(20);
+      const expires = Date.now() + 1000 * 60 * 60;
+      const token = {
+        phone: phone.trim(),
+        id,
+        expires
+      };
+
+      dataStore.create('tokens', id, token, err => {
+        if (err) {
+          callback(500, { error: "Error to create user token" });
+          return;
+        }
+
+        callback(200, token);
+      });
+
+
+
+    });
   }
 
   const tokenPut = () => {
